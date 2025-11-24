f0d7e7dbe584a8f3f1b39f888c8185c37933ac94
AminaIslam1912 <jenny191277@gmail.com>
2025-11-24 14:11:32 +0600

    ki j banai nijei jani na

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+
+function Home() {
+
+  const [category, setCategory] = React.useState('All');
+
+
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  )
+}
+
+export default Home
