dcd2d67d9815cf80fe374c8a4f973199fa6ce967
AminaIslam1912 <jenny191277@gmail.com>
2025-11-25 22:03:06 +0600

    make responsive

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+
+const AppDownload = () => {
+  return (
+    <div className='app-download' id='app-download'>
+
+      <p>For better experience, download <br />Tomato app!</p>
+     
+        <div className="app-download-platform">
+          <img src={assets.play_store} alt="Play Store" />
+          <img src={assets.app_store} alt="App Store" />
+           </div>
+      
+    </div>
+  )
+}
+
+export default AppDownload
